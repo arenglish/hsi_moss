85c8826cbf48fbfda6d9fd1ea98b419cef444374
arenglish <austinenglish4@gmail.com>
2023-02-18 13:05:32 +0100

    add envi_to_stiff

---
+++ src/hsi_moss/hsi_prepare.py
@@ -0,0 +1,17 @@
+from pathlib import Path, PurePath
+from typing import SupportsInt, AnyStr
+# importing PIL
+from PIL import Image
+from spectral_envi import *
+from spectral_tiffs import write_stiff
+ 
+
+def open_image(path: PurePath):
+    return open(path.absolute())
+
+def envi_to_stiff(hdrPath: PurePath, tiffPath: PurePath = None):
+    if tiffName is None:
+        tiffName = hdrPath.with_suffix('.tif')
+        
+    im, wavelengths = read_envi(hdrPath.absolute())
+    write_stiff(tiffPath.name, im, wavelengths)
