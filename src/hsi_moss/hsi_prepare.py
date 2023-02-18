58b1ede9d03c1fc994f945ba67ede564f4243ba2
arenglish <austinenglish4@gmail.com>
2023-02-18 14:05:16 +0100

    correct method name

--- src/hsi_moss/hsi_prepare.py
+++ src/hsi_moss/hsi_prepare.py
@@ -3,14 +3,10 @@ from typing import SupportsInt, AnyStr
 # importing PIL
 from hsi_moss.spectral_envi import *
 from hsi_moss.spectral_tiffs import write_stiff
- 
-
-def open_image(path: PurePath):
-    return open(path.absolute())
 
 def envi_to_stiff(hdrPath: PurePath, tiffPath: PurePath = None):
     if tiffPath is None:
         tiffPath = hdrPath.with_suffix('.tif')
         
-    im, wavelengths = read_envi(hdrPath.absolute())
-    write_stiff(tiffPath.name, im, wavelengths)
+    im, wavelengths = read_envi(hdrPath.as_posix())
+    write_stiff(tiffPath.as_posix(), im, wavelengths, None)
